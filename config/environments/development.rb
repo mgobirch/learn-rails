f464108f1f28555a41efa9ef447d2d5a00ac551b
Bryan Birchmeier <birch1bs@gmail.com>
2016-03-23 18:31:29 -0400

    created interactive contact form, which I actually got to work.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -29,19 +29,19 @@ Rails.application.configure do
     config.action_mailer.smtp_settings = {
       address: "smtp.gmail.com",
       port: 587,
-      domain: Rails.application.secrets.domain_name,
+      domain: ENV["DOMAIN_NAME"],
       authentication: "plain",
       enable_starttls_auto: true,
-      user_name: Rails.application.secrets.email_provider_username,
-      password: Rails.application.secrets.email_provdier_password
+      user_name: ENV["GMAIL_USERNAME"],
+      password: ENV["GMAIL_PASSWORD"]
     }
   # ActionMailer Config
   config.action_mailer.default_url_options = { :host => 'localhost:3000' }
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.raise_delivery_errors = false
 
   # Send email in development mode?
-  #config.action_mailer.perform_deliveries = true
+  config.action_mailer.perform_deliveries = true
 
   # Asset digests allow you to set far-future HTTP expiration dates on all assets,
   # yet still be able to expire them through the digest params.
